aad5d88203a1186dc499d23d74a047877cab8643
ruscoe <danruscoe@protonmail.com>
2024-07-05 18:33:34 -0700

    Be consistent with variable names.

--- maze.py
+++ maze.py
@@ -13,30 +13,30 @@ from src.texture import Texture
 window = 0
 
 # Size of cubes used to create wall segments.
-cubesize = 2
+cube_size = 2
 # Space around cubes to extend hitbox (prevents peeking through walls).
 collision_padding = 0.5
 # Initial camera position after map is drawn.
-camerapos = [-8.0, 0.0, -38.0]
+camera_pos = [-8.0, 0.0, -38.0]
 # camerapos = [0.0, 0.0, 0.0]
 # Initial camera rotation.
-camerarot = 0.0
+camera_rot = 0.0
 # The angle in degrees the camera rotates each turn.
-rotate_angle = 1;
+rotate_angle = 1
 
 first_run = False
 
 collision = Collision()
-input = Input();
-movement = Movement();
+input = Input()
+movement = Movement()
 
 map = []
 
 # Loaded textures.
-ceilingtexture = None
-floortexture = None
-orbtexture = None
-walltexture = None
+ceiling_texture = None
+floor_texture = None
+orb_texture = None
+wall_texture = None
 
 def initGL(Width, Height):
 
@@ -52,7 +52,7 @@ def initGL(Width, Height):
 
 def drawScene():
 
-    global camerapos, first_run
+    global camera_pos, first_run
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
@@ -65,14 +65,14 @@ def drawScene():
     # Set up the current maze view.
     # Reset position to zero, rotate around y-axis, restore position.
     glTranslatef(0.0, 0.0, 0.0)
-    glRotatef(camerarot, 0.0, 1.0, 0.0)
-    glTranslatef(camerapos[0], camerapos[1], camerapos[2])
+    glRotatef(camera_rot, 0.0, 1.0, 0.0)
+    glTranslatef(camera_pos[0], camera_pos[1], camera_pos[2])
 
     # Draw floor.
     glPushMatrix()
     glTranslatef(0.0, -2.0, 0.0)
     glScalef(30.0, 1.0, 30.0)
-    plane.drawplane(floortexture, 10.0)
+    plane.drawplane(floor_texture, 10.0)
     glPopMatrix()
 
     # Draw ceiling.
@@ -80,16 +80,16 @@ def drawScene():
     glTranslatef(0.0, 2.0, 0.0)
     glRotatef(180.0, 0.0, 0.0, 1.0)
     glScalef(30.0, 1.0, 30.0)
-    plane.drawplane(ceilingtexture, 10.0)
+    plane.drawplane(ceiling_texture, 10.0)
     glPopMatrix()
 
     # Draw test sprite.
     glPushMatrix()
     glTranslatef(0.0, 0.5, -6.0)
     glRotatef(90.0, 1.0, 0.0, 0.0)
-    glRotatef(camerarot, 0.0, 0.0, 1.0)
+    glRotatef(camera_rot, 0.0, 0.0, 1.0)
     glScalef(1.0, 0.0, 1.0)
-    sprite.drawSprite(orbtexture)
+    sprite.drawSprite(orb_texture)
     glPopMatrix()
 
     # Build the maze like a printer; back to front, left to right.
@@ -101,27 +101,27 @@ def drawScene():
 
     for i in map:
 
-        wall_z = (row_count * (cubesize * -1))
+        wall_z = (row_count * (cube_size * -1))
 
         for j in i:
 
             # 1 = cube, 0 = empty space.
             if (j == 1):
-                cube.drawcube(walltexture, 1.0)
+                cube.drawcube(wall_texture, 1.0)
 
-                wall_x = (column_count * (cubesize * -1))
+                wall_x = (column_count * (cube_size * -1))
 
                 if (first_run != True):
                     print('Drawing cube at X:', wall_x, 'Z:', wall_z)
 
             # Move from left to right one cube size.
-            glTranslatef(cubesize, 0.0, 0.0)
+            glTranslatef(cube_size, 0.0, 0.0)
 
             column_count += 1
 
         # Reset position before starting next row, while moving
         # one cube size towards the camera.
-        glTranslatef(((cubesize * column_count) * -1), 0.0, cubesize)
+        glTranslatef(((cube_size * column_count) * -1), 0.0, cube_size)
 
         row_count += 1
         # Reset the column count; this is a new row.
@@ -136,55 +136,55 @@ def drawScene():
 
 def handleInput():
 
-    global input, camerapos, camerarot
+    global input, camera_pos, camera_rot
 
     if input.isKeyDown(Input.KEY_STATE_ESCAPE):
         sys.exit()
 
     if input.isKeyDown(Input.KEY_STATE_LEFT):
-        camerarot -= rotate_angle
+        camera_rot -= rotate_angle
 
     if input.isKeyDown(Input.KEY_STATE_RIGHT):
-        camerarot += rotate_angle
+        camera_rot += rotate_angle
 
-    intended_pos = [camerapos[0], 0, camerapos[2]];
+    intended_pos = [camera_pos[0], 0, camera_pos[2]]
 
     if input.isKeyDown(Input.KEY_STATE_FORWARD) or input.isKeyDown(Input.KEY_STATE_BACK):
         modifier = 1 if input.isKeyDown(Input.KEY_STATE_FORWARD) else -1
-        intended_pos = movement.getIntendedPosition(camerarot, camerapos[0], camerapos[2], 90, modifier)
+        intended_pos = movement.getIntendedPosition(camera_rot, camera_pos[0], camera_pos[2], 90, modifier)
 
     if input.isKeyDown(Input.KEY_STATE_LEFT_STRAFE) or input.isKeyDown(Input.KEY_STATE_RIGHT_STRAFE):
         modifier = 1 if input.isKeyDown(Input.KEY_STATE_LEFT_STRAFE) else -1
-        intended_pos = movement.getIntendedPosition(camerarot, camerapos[0], camerapos[2], 0, modifier)
+        intended_pos = movement.getIntendedPosition(camera_rot, camera_pos[0], camera_pos[2], 0, modifier)
 
     intended_x = intended_pos[0]
     intended_z = intended_pos[2]
 
     # Detect collision with walls.
-    if (collision.testCollision(cubesize, map, intended_x, intended_z, collision_padding)):
+    if (collision.testCollision(cube_size, map, intended_x, intended_z, collision_padding)):
         print('Collision at X:', intended_x, 'Z:', intended_z)
 
         # If it's possible to keep the user moving by sliding along the wall, do so.
         slide_time = False
-        if (collision.testCollision(cubesize, map, intended_x, camerapos[2], collision_padding) == False):
-            intended_z = camerapos[2]
+        if (collision.testCollision(cube_size, map, intended_x, camera_pos[2], collision_padding) == False):
+            intended_z = camera_pos[2]
             slide_time = True
-        elif (collision.testCollision(cubesize, map, camerapos[0], intended_z, collision_padding) == False):
-            intended_x = camerapos[0]
+        elif (collision.testCollision(cube_size, map, camera_pos[0], intended_z, collision_padding) == False):
+            intended_x = camera_pos[0]
             slide_time = True
 
         if (slide_time):
             # Slide the camera along the wall.
-            camerapos[0] = intended_x
-            camerapos[2] = intended_z
+            camera_pos[0] = intended_x
+            camera_pos[2] = intended_z
     else:
         # Move the camera to the user's intended position.
-        camerapos[0] = intended_x
-        camerapos[2] = intended_z
+        camera_pos[0] = intended_x
+        camera_pos[2] = intended_z
 
 def main():
 
-    global window, ceilingtexture, floortexture, orbtexture, walltexture, map
+    global window, ceiling_texture, floor_texture, orb_texture, wall_texture, map
 
     glutInit(sys.argv)
     glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
@@ -220,10 +220,10 @@ def main():
     # Load texture.
     texture = Texture()
 
-    ceilingtexture = texture.loadImage('tex/ceiling.png')
-    floortexture = texture.loadImage('tex/floor.png')
-    orbtexture = texture.loadImage('tex/orb.png')
-    walltexture = texture.loadImage('tex/wall.png')
+    ceiling_texture = texture.loadImage('tex/ceiling.png')
+    floor_texture = texture.loadImage('tex/floor.png')
+    orb_texture = texture.loadImage('tex/orb.png')
+    wall_texture = texture.loadImage('tex/wall.png')
 
     glutIgnoreKeyRepeat(1)
     glutKeyboardFunc(input.registerKeyDown)
--- src/texture.py
+++ src/texture.py
@@ -13,16 +13,16 @@ class Texture:
 
         print('Opened image file: size =', image.size, 'format =', image.format)
 
-        textureID = glGenTextures(1)
+        texture_id = glGenTextures(1)
         glPixelStorei(GL_UNPACK_ALIGNMENT, 4)
-        glBindTexture(GL_TEXTURE_2D, textureID)
+        glBindTexture(GL_TEXTURE_2D, texture_id)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_BASE_LEVEL, 0)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0)
 
-        imageData = image.convert('RGBA').tobytes()
+        image_data = image.convert('RGBA').tobytes()
 
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.size[0], image.size[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, imageData)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.size[0], image.size[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data)
 
         image.close()
 
-        return textureID
+        return texture_id
